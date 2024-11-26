2cb0c6f87c7b2821aae96009d106375b64d8c44b
Amber Taveras <taveras.amber@gmail.com>
2024-11-26 17:38:21 -0500

    adds options to display sidebar as grouping or regular array

--- src/app/api/flashcards/route.ts
+++ src/app/api/flashcards/route.ts
@@ -6,51 +6,11 @@ export async function GET(request: Request) {
     try {
         await connectToDatabase();
 
-        const aggregatedFlashcardSets = await FlashcardSet.aggregate([
-            // Step 1: Group by subject and topic
-            {
-                $group: {
-                    _id: {
-                        subject: '$subject',
-                        topic: '$topic'
-                    },
-                    flashcardSets: {
-                        // includes flashcardSets name and _id to generate links
-                        $push: {
-                            name: '$name',
-                            _id: '$_id'
-                        }
-                    },
-                },
-            },
-            // Step 2: Sort topics alphabetically within each subject
-            {
-                $sort: {
-                    '_id.subject': 1,
-                    '_id.topic': 1
-                },
-            },
-            // Step 3: Group by subject and format it to contain topics and flashcard sets
-            {
-                $group: {
-                    _id: '$_id.subject',
-                    topics: {
-                        $push: {
-                            topic: '$_id.topic',
-                            flashcardSets: '$flashcardSets'
-                        },
-                    },
-                },
-            },
-            // Final Sort: Sort the subjects alphabetically
-            {
-                $sort: {
-                    '_id': 1,
-                },
-            },
-        ]);
+        const url = new URL(request.url);
+        const sortBy = url.searchParams.get('sortBy') || 'mostRecent';
 
-        return NextResponse.json(aggregatedFlashcardSets);
+        const flashcardSets = sortBy === 'mostRecent' ? await getByMostRecent() : await aggregateBySubject();
+        return NextResponse.json(flashcardSets);
 
     } catch (error) {
         console.error('Error creating flashcard set:', error);
@@ -58,5 +18,63 @@ export async function GET(request: Request) {
             { error: 'Failed to create flashcard set and flashcards' },
             { status: 500 }
         );
+    }
+}
+
+async function aggregateBySubject() {
+    const aggregatedFlashcardSets = await FlashcardSet.aggregate([
+        // Step 1: Group by subject and topic
+        {
+            $group: {
+                _id: {
+                    subject: '$subject',
+                    topic: '$topic'
+                },
+                flashcardSets: {
+                    // includes flashcardSets name and _id to generate links
+                    $push: {
+                        name: '$name',
+                        _id: '$_id'
+                    }
+                },
+            },
+        },
+        // Step 2: Sort topics alphabetically within each subject
+        {
+            $sort: {
+                '_id.subject': 1,
+                '_id.topic': 1
+            },
+        },
+        // Step 3: Group by subject and format it to contain topics and flashcard sets
+        {
+            $group: {
+                _id: '$_id.subject',
+                topics: {
+                    $push: {
+                        topic: '$_id.topic',
+                        flashcardSets: '$flashcardSets'
+                    },
+                },
+            },
+        },
+        // Final Sort: Sort the subjects alphabetically
+        {
+            $sort: {
+                '_id': 1,
+            },
+        },
+    ]);
+
+    return aggregatedFlashcardSets;
+}
+
+async function getByMostRecent () {
+    try {
+        const flashcardSets = await FlashcardSet.find({}).sort({ createdAt: -1 });
+        return flashcardSets;
+    } catch (error) {
+        console.error('Error fetching flashcard sets:', error);
+        throw error;
     }
 }
